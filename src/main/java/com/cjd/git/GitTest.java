8db798f38db9791a65bf60d491484a0e701cbdac
cjd <1062215981@qq.com>
2022-08-10 11:30:55 +0800

    first commit

---
+++ src/main/java/com/cjd/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.cjd.git;
+
+/**
+ * @author Cjd
+ * @create 2022-08-10  11:15
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
